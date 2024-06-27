3b3b75fbbd849674b8ad0fa5030b02d8fc985bd8
Chip <chip@Chips-MacBook-Air.local>
2024-06-26 21:35:50 -0400

    initial commit

---
+++ DateApp/DateApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DateApp
+//
+//  Created by Cliff Baeseman on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("DateBase!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DateApp/DateApp/DateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateAppApp.swift
+//  DateApp
+//
+//  Created by Chip on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
